049fe590c0d162ded3bbb2eeb96f32f606df3aae
Chang Chen Ong <twt6m@hotmail.com>
2019-02-12 14:27:47 +0000

    Added RecycleView dependency

--- app/src/main/java/com/example/c7_ong/comphouse/FetchInfo.java
+++ app/src/main/java/com/example/c7_ong/comphouse/FetchInfo.java
@@ -2,7 +2,7 @@ package com.example.c7_ong.comphouse;
 
 import android.os.AsyncTask;
 import android.widget.TextView;
-
+import android.support.v7.widget.RecyclerView;
 import java.lang.ref.WeakReference;
 
 public class FetchInfo extends AsyncTask <String, Void, String>{
