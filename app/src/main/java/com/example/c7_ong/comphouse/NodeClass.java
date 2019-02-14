2a74f90b386c8cb6938062e7397e1684f8da4b03
changchen96 <twt6m@hotmail.com>
2019-02-14 18:32:07 +0000

    Fixed node drawing, but improvements are needed

--- app/src/main/java/com/example/c7_ong/comphouse/NodeClass.java
+++ app/src/main/java/com/example/c7_ong/comphouse/NodeClass.java
@@ -1,11 +1,22 @@
 package com.example.c7_ong.comphouse;
 
+import android.graphics.Point;
+
 public class NodeClass {
     private float radius = 50;
+    private Point nodePoint;
+    int x;
+    int y;
 
     public float getRadius()
     {
         return radius;
     }
 
+    public Point setPoint(int PointX, int PointY)
+    {
+        this.x = PointX;
+        this.y = PointY;
+        return nodePoint;
+    }
 }
--- app/src/main/java/com/example/c7_ong/comphouse/drawGraph.java
+++ app/src/main/java/com/example/c7_ong/comphouse/drawGraph.java
@@ -14,31 +14,50 @@ public class drawGraph extends AppCompatActivity {
 
     private Bitmap mBitmap;
     private Canvas mCanvas;
-    private Paint mPaint;
+    private Paint mPaint1;
+    private Paint mPaint2;
     private NodeClass newNodeClass;
     private ImageView mImageView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_draw_graph);
         mImageView = (ImageView) findViewById(R.id.graphView);
-        drawGraph(mImageView);
     }
 
     public void drawGraph(View view) {
-        int height = view.getHeight();
-        int width = view.getWidth();
-        int x = 1;
-        int y = 1;
+        int height = 1000;
+        int width = 1000;
+        int OldX = 100;
+        int OldY = 70;
+        int counter;
+        int NewY = OldY;
+        int NewX = OldX;
+        newNodeClass = new NodeClass();
         mBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
         mCanvas = new Canvas(mBitmap);
-        mPaint.setColor(Color.BLUE);
+        mPaint1 = new Paint();
+        mPaint1.setColor(Color.BLUE);
+        mPaint2 = new Paint();
+        mPaint2.setColor(Color.BLACK);
         float circleRadius = newNodeClass.getRadius();
-        for (y = 1; y < 6; y++) {
-            mCanvas.drawCircle(x, y, circleRadius, mPaint);
+        for (counter = 0; counter < 5; counter++)
+        {
+            mCanvas.drawCircle(OldX, NewY, circleRadius, mPaint1);
+            mCanvas.drawLine(OldX, OldY, NewX, NewY, mPaint2);
+            NewY = NewY + 200;
+            OldY = NewY;
         }
+        mImageView.setImageBitmap(mBitmap);
         view.invalidate();
 
+    }
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        drawGraph(mImageView);
     }
 }
